6be41a7975cf10f7e8742f08d07418c6d77ca5e2
Matthias Kestenholz <mk@spinlock.ch>
2013-03-13 12:46:07 +0100

    Additional field full_name for testing

--- newsletter_subscription/models.py
+++ newsletter_subscription/models.py
@@ -5,7 +5,11 @@ from django.utils.translation import ugettext_lazy as _
 class Subscription(models.Model):
     email = models.EmailField(_('email address'), max_length=254, unique=True)
     is_active = models.BooleanField(_('is active'), default=False)
+    full_name = models.CharField(_('full name'), max_length=100, blank=True)
 
     class Meta:
         verbose_name = _('subscription')
         verbose_name_plural = _('subscriptions')
+
+    def __unicode__(self):
+        return self.email
