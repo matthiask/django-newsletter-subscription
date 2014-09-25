6fcc4b62816ad1f7084685081dadc185085ab907
Matthias Kestenholz <mk@spinlock.ch>
2014-09-25 14:27:33 +0200

    __str__

--- newsletter_subscription/models.py
+++ newsletter_subscription/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 
+@python_2_unicode_compatible
 class SubscriptionBase(models.Model):
     email = models.EmailField(_('email address'), max_length=254, unique=True)
     is_active = models.BooleanField(_('is active'), default=False)
@@ -11,5 +13,5 @@ class SubscriptionBase(models.Model):
         verbose_name = _('subscription')
         verbose_name_plural = _('subscriptions')
 
-    def __unicode__(self):
+    def __str__(self):
         return self.email
