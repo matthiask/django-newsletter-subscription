507901f319caf5e32cf165120b6d337f249326cc
Matthias Kestenholz <mk@feinheit.ch>
2016-01-18 10:57:02 +0100

    Reformat urls.py, pass functions to url()

--- newsletter_subscription/urls.py
+++ newsletter_subscription/urls.py
@@ -1,16 +1,26 @@
 from django.conf.urls import patterns, url
 
+from newsletter_subscription.views import form, subscribe, resubscribe
+
 
 def newsletter_subscriptions_urlpatterns(**kwargs):
-    return patterns(
-        'newsletter_subscription.views',
-        url(r'^$', 'form', kwargs,
+    return [
+        url(
+            r'^$',
+            form,
+            kwargs,
             name='newsletter_subscription_form',
-            ),
-        url(r'^s/(?P<code>[^/]+)/$', 'subscribe', kwargs,
+        ),
+        url(
+            r'^s/(?P<code>[^/]+)/$',
+            subscribe,
+            kwargs,
             name='newsletter_subscription_subscribe',
-            ),
-        url(r'^r/(?P<code>[^/]+)/$', 'resubscribe', kwargs,
+        ),
+        url(
+            r'^r/(?P<code>[^/]+)/$',
+            resubscribe,
+            kwargs,
             name='newsletter_subscription_resubscribe',
-            ),
-        )
+        ),
+    ]
